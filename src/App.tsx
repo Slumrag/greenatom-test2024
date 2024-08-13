085dda66a32e0e04507b8bc8e5e62cbf1d29c03c
Slumrag <mrgreenthecristal@yandex.ru>
2024-08-13 18:33:08 +0300

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import { observer } from 'mobx-react-lite';
 import './App.css';
 import { ImageGallery } from './components/ImageGallery/ImageGallery';
 import { MainStoreProvider } from './store/MainStore/MainStoreProvider';
+import { Container, Nav, Navbar, Stack } from 'react-bootstrap';
 
 const App = observer(() => {
   return (
-    <MainStoreProvider>
-      <ImageGallery></ImageGallery>
-    </MainStoreProvider>
+    <>
+      <Navbar>
+        <Container>
+          <Nav>
+            <Nav.Link href='http://localhost:8055'>Панель Администратора</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Stack gap={4}>
+        <MainStoreProvider>
+          <ImageGallery></ImageGallery>
+        </MainStoreProvider>
+      </Stack>
+    </>
   );
 });
 
